9121cceafcde4b9e3c6e285f6a9859675ad45c09
Yisus <jesustellez.contacto@gmail.com>
2021-08-21 16:35:06 +0200

    Usable Release 1.0

---
+++ App.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace RockstarCloudXMLToFivemJSON
+{
+    partial class App
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openXMLbtn = new System.Windows.Forms.Button();
+            this.saveJSONbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openXMLbtn
+            // 
+            this.openXMLbtn.Location = new System.Drawing.Point(3, 53);
+            this.openXMLbtn.Name = "openXMLbtn";
+            this.openXMLbtn.Size = new System.Drawing.Size(141, 30);
+            this.openXMLbtn.TabIndex = 0;
+            this.openXMLbtn.Text = "Open XML";
+            this.openXMLbtn.UseVisualStyleBackColor = true;
+            this.openXMLbtn.Click += new System.EventHandler(this.openXMLbtn_Click);
+            // 
+            // saveJSONbtn
+            // 
+            this.saveJSONbtn.Location = new System.Drawing.Point(253, 53);
+            this.saveJSONbtn.Name = "saveJSONbtn";
+            this.saveJSONbtn.Size = new System.Drawing.Size(141, 30);
+            this.saveJSONbtn.TabIndex = 1;
+            this.saveJSONbtn.Text = "Save JSON";
+            this.saveJSONbtn.UseVisualStyleBackColor = true;
+            this.saveJSONbtn.Click += new System.EventHandler(this.saveJSONbtn_Click);
+            //
+            // saveFileDialog
+            //
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            // 
+            // Title Label
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(75, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GTA V Cloud XML to FiveM JSON";
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 95);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveJSONbtn);
+            this.Controls.Add(this.openXMLbtn);
+            this.Name = "App";
+            this.Text = "GTA V Cloud XML to JSON";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openXMLbtn;
+        private System.Windows.Forms.Button saveJSONbtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ App.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace RockstarCloudXMLToFivemJSON
+{
+    public partial class App : Form
+    {
+        private List<String> xmlList;
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void openXMLbtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            SetOpenXMLDialogSettings(openFileDialog1);
+            openFileDialog1.ShowDialog();
+            ConvertXMLToJSON(openFileDialog1.FileName);
+        }
+        private void saveJSONbtn_Click(object sender, EventArgs e)
+        {
+            String bigStr = "<GTARace>";
+            if (xmlList != null && xmlList.Count > 0)
+            {
+                // Write big string with all xml data
+                foreach (String text in xmlList)
+                {
+                    bigStr += text;
+                }
+                bigStr += "</GTARace>";
+
+                // Convert XML to JSON and Save It
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(bigStr);
+                String json = JsonConvert.SerializeXmlNode(doc);
+
+                // Finally save usable file
+                SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+                SetSaveJSONDialogSettings(SaveFileDialog1);
+                
+                if(SaveFileDialog1.ShowDialog() == DialogResult.OK) {
+                    String savePath = SaveFileDialog1.FileName;
+                    using (StreamWriter writer = new StreamWriter(savePath))
+                    {
+                        writer.WriteLine(json);
+                        MessageBox.Show("File saved succesfully.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error, the XML is not a valid GTA V Cloud XML file, please try another file.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, the XML is not a valid GTA V Cloud XML file, please try another file.");
+            }
+        }
+
+        private void SetSaveJSONDialogSettings(SaveFileDialog SaveFileDialog1)
+        {
+            SaveFileDialog1.Title = "Save Usable JSON File";
+            SaveFileDialog1.InitialDirectory = @"C:\";
+            SaveFileDialog1.DefaultExt = "json";
+            SaveFileDialog1.Filter = "json files (*.json)|*.json";
+            SaveFileDialog1.FilterIndex = 1;
+            SaveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.RestoreDirectory = true;
+        }
+
+        private void SetOpenXMLDialogSettings(OpenFileDialog dialog)
+        {
+            dialog.Title = "Browse and select a valid XML GTA V Cloud File";
+            dialog.DefaultExt = "xml";
+            dialog.Filter = "xml files (*.xml)|*.xml";
+            dialog.FilterIndex = 1;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+        }
+
+        private void ConvertXMLToJSON(String fPath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fPath);
+            xmlList = new List<String>();
+            XmlNodeList elemList = doc.GetElementsByTagName("SpoonerPlacements");
+            Debug.WriteLine(elemList);
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                String toAdd = elemList[i].InnerXml;
+                xmlList.Add(toAdd);
+            }
+        }
+    }
+}
